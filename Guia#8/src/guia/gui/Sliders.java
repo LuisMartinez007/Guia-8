1025da89ffaa4e52a71b372bbb60075b6ba9640f
Luis <luis.martinezlopez@unicolombo.edu.co>
2024-04-23 11:31:51 -0500

    Sliders

---
+++ Guia#8/src/guia/gui/Sliders.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package guia.gui;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Sliders {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Main principal = new Main();
+        
+        principal.setLocationRelativeTo(null);
+        principal.setVisible(true);
+    }
+    
+}
